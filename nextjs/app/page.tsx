b0b19d22ebf2905046ca7f0304d4c012e000ca09
stephendandrea <sadandrea@gmail.com>
2024-09-28 12:53:23 -0400

    test trigger

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -4,6 +4,7 @@ import { Uploader } from './components/uploader';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Pork chops and apple sauce
       <Uploader />
     </main>
   );
