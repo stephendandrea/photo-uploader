8b404ba2d3e03a5b5bf42159267dd8372fafb8bb
stephendandrea <sadandrea@gmail.com>
2024-10-12 12:19:46 -0400

    update

--- nextjs/pages/users.tsx
+++ nextjs/pages/users.tsx
@@ -3,16 +3,13 @@ import '@/app/globals.css';
 import { User } from '@prisma/client';
 
 export async function getStaticProps() {
-  // const users = await prisma.user.findMany();
+  const users = await prisma.user.findMany();
 
-  const users = await prisma.$queryRaw`SELECT * FROM User`;
-
-  console.log({ users });
   return {
     props: { users },
   };
 }
-export default function Home({ users }: { users: User[] }) {
+export default function Home({ users }: {users: User[] }) {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ul>
