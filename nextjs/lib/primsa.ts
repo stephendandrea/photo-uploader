e5ea09a838423ffab1346964f52063b63bd7e991
Stephen D'Andrea <sadandrea@gmail.com>
2025-08-06 13:53:16 -0400

    test commit (#1)

--- nextjs/lib/primsa.ts
+++ nextjs/lib/primsa.ts
@@ -1,13 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 interface CustomNodeJsGlobal {
   prisma: PrismaClient;
 }
 
+// comment
 declare const global: CustomNodeJsGlobal;
 
 const prisma = new PrismaClient();
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+if (process.env.NODE_ENV === "development") global.prisma = prisma;
 
 export default prisma;
