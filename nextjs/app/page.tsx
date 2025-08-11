161b510bd7abecd03aab7070fbaf5e3aac9430c8
Stephen D'Andrea <stephen@trycoast.com>
2025-08-11 14:39:21 -0400

    no message

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,5 +1,6 @@
-import { Uploader } from './components/uploader';
+import { Uploader } from "./components/uploader";
 
+// Feature 6
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
